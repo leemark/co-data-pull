884a4cfb00ce41f31b19a39ee20f235cf7d1591b
leemark <leemark@gmail.com>
2014-03-25 17:28:55 -0600

    formatted js

--- js/pull.js
+++ js/pull.js
@@ -1,13 +1,14 @@
 (function($){
     $('button').on('click', function(){
-        var zip = $( "select option:selected" ).text().substring(0,6);
-        $.getJSON( "http://data.colorado.gov/resource/4ykn-tg5h.json?entityStatus=Good%20Standing&principalZipCode=" + zip, function( data ) {
-          var items = [];
-          $.each( data, function( key, val ) {
+        var zip = $("select option:selected").text().substring(0,6);
+        $.getJSON( "http://data.colorado.gov/resource/4ykn-tg5h.json?entityStatus=Good%20Standing&principalZipCode=" + zip, function(data) {
+            var items = [];
+            var $ul;  
+            $.each(data, function(key, val){
               items.push( "<li id='" + key + "'><span class='name'>" + val.entityname + "</span> <span class='addr'>" + val.principaladdress1 + "</span> <span class='city'>" + val.principalcity + "</span></li>" );
-          });
-            $('<ul />').appendTo('.content');
-            $('ul').append(items);
+            });
+            $ul = $('<ul />').appendTo('.content');
+            $ul.append(items);
         });
     });
 }(jQuery));
