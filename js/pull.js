e79bcac45d4c663e3e926e9658f879a98bf05386
leemark <leemark@gmail.com>
2014-03-25 22:18:57 -0600

    added comments

--- js/pull.js
+++ js/pull.js
@@ -1,16 +1,33 @@
 (function($){
     $('button').on('click', function(){
+        // remove resultset if this has already been run
         $('.content ul').remove();
+        // add spinner to indicate something is happening
         $('<i class="fa fa-refresh fa-spin"/>').appendTo('body');
+        
+        // get selected zip code from selectbox
         var zip = $("select option:selected").text().substring(0,6);
+        
+        // make AJAX call
         $.getJSON( "http://data.colorado.gov/resource/4ykn-tg5h.json?entityStatus=Good%20Standing&principalZipCode=" + zip, function(data) {
-            var items = [];
+            
+            // do all this on success
+            
+            var items = []; 
             var $ul;  
+            
             $.each(data, function(key, val){
-              items.push( "<li id='" + key + "'><span class='name'>" + val.entityname + "</span><br><span class='addr'>" + val.principaladdress1 + "</span> <span class='city'>" + val.principalcity + "</span></li>" );
+                //iterate through the returned data and build a list
+                items.push( "<li id='" + key + "'><span class='name'>" + val.entityname + "</span><br><span class='addr'>" + val.principaladdress1 + "</span> <span class='city'>" + val.principalcity + "</span></li>" );
             });
+            
+            // remove spinner
             $('.fa-spin').remove();
+            
+            // append list to page
             $ul = $('<ul />').appendTo('.content');
+            
+            //append list items to list
             $ul.append(items);
         });
     });
