6b39acd14c79966f446def96478067083a78df59
leemark <leemark@gmail.com>
2014-03-30 23:01:02 -0600

    linted

--- js/pull.js
+++ js/pull.js
@@ -1,28 +1,27 @@
-(function($){
-    $('button').on('click', function(){
+(function ($) {
+    $('button').on('click', function () {
         // remove resultset if this has already been run
         $('.content ul').remove();
         // add spinner to indicate something is happening
         $('<i class="fa fa-refresh fa-spin"/>').appendTo('body');
         
         // get selected zip code from selectbox
-        var zip = $("select option:selected").text().substring(0,6);
+        var zip = $('select option:selected').text().substring(0, 6);
         
         // make AJAX call
-        $.getJSON( "http://data.colorado.gov/resource/4ykn-tg5h.json?entityStatus=Good%20Standing&principalZipCode=" + zip, function(data) {
+        $.getJSON('http://data.colorado.gov/resource/4ykn-tg5h.json?entityStatus=Good%20Standing&principalZipCode=' + zip, function (data) {
             
-            // do all this on success
+            // do all this on success       
+            var items = [],
+                $ul;
             
-            var items = []; 
-            var $ul;  
-            
-            $.each(data, function(key, val){
+            $.each(data, function (key, val) {
                 //iterate through the returned data and build a list
-                items.push( "<li id='" + key + "'><span class='name'>" + val.entityname + "</span><br><span class='addr'>" + val.principaladdress1 + "</span> <span class='city'>" + val.principalcity + "</span></li>" );
+                items.push('<li id="' + key + '"><span class="name">' + val.entityname + '</span><br><span class="addr">' + val.principaladdress1 + '</span> <span class="city">' + val.principalcity + '</span></li>');
             });
             // if no items were returned then add a message to that effect
-            if(items.length < 1){
-                items.push( "<li>No results for this ZIP code, try again!</li>" );
+            if (items.length < 1) {
+                items.push('<li>No results for this ZIP code, try again!</li>');
             }
             
             // remove spinner
